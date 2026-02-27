62c99a09c9a17bbb38b12175faa14731a59a42a7
Brian Aira <136931601+BrianAira@users.noreply.github.com>
2026-02-27 19:52:19 -0300

    fix(user): corregir puerto de contrato de metodo de usuario

--- app/domain/ports/user_repository.py
+++ app/domain/ports/user_repository.py
@@ -5,7 +5,7 @@ from app.domain.models.directions import Directions
 from app.domain.models.user import User
 from app.domain.schemas.directions import DirectionCreate
 from app.domain.schemas.user import UserUpdate
-from app.schemas.user import UserCreate
+from app.domain.schemas.user import UserCreate
 
 
 class IUserRepository(ABC):
@@ -25,9 +25,9 @@ class IUserRepository(ABC):
     def update(self, user_id:int, user_data:UserUpdate)->Optional[User]:
         pass
     
-    @abstractmethod 
-    def list_all(self, skip:int=0, limit:int=10)->List[User]:
-        pass
+    # @abstractmethod 
+    # def list_all(self, skip:int=0, limit:int=10)->List[User]:
+    #     pass
     
     @abstractmethod
     def add_address(self, user_id:int, address_data:DirectionCreate):
@@ -35,5 +35,9 @@ class IUserRepository(ABC):
     
     @abstractmethod
     def get_addresses(self, user_id:int)->List[Directions]:
+        pass
+    
+    @abstractmethod
+    def get_address_by_id(self, address_id:int)->Optional[Directions]:
         pass
     
