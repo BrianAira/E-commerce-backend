024b54afbced16c56e5fec18cfee8bf360edddc1
Brian Aira <136931601+BrianAira@users.noreply.github.com>
2026-02-27 20:05:34 -0300

    fix(config):corregir metodos de hashing

--- app/core/config.py
+++ app/core/config.py
@@ -5,7 +5,8 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     PROJECT_NAME:str="Mi Odisea API"
-    DATABASE_URL:str = "mysql+mysqlconnector://user:secret@localhost:3306/final"
+    # DATABASE_URL:str = "mysql+mysqlconnector://user:secret@localhost:3306/final"
+    DATABASE_URL:str = "postgresql+psycopg2://myuser:mypassword@localhost:5433/apiBrian"
 
     SECRET_KEY:str="super_secret_key"
     ALGORITHM:str="HS256"
