fd905aec3168a1e0b7d58879daa3d24fb409ec2e
Brian Aira <136931601+BrianAira@users.noreply.github.com>
2026-02-27 19:41:30 -0300

    fix(category): corregir modelo de categorias

---
+++ app/domain/models/category.py
@@ -0,0 +1,17 @@
+
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
+
+from app.core.database import Base
+
+
+class Category(Base):
+    __tablename__="categories"
+    
+    id=Column(Integer, primary_key=True, index=True)
+    name=Column(String(255), unique=True, nullable=False)
+    
+    slug=Column(String(120), unique=True, index=True, nullable=False)
+    
+    
+    products=relationship("Product", back_populates="category")
