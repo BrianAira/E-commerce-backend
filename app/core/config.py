dfa8c17c0fb58c6cc558e7798fee3c14122ea1bc
Brian Aira <136931601+BrianAira@users.noreply.github.com>
2026-03-02 20:02:00 -0300

    fix(config):declarar configuracion para extraer desde archivo env

--- app/core/config.py
+++ app/core/config.py
@@ -1,17 +1,31 @@
 from typing import List
 
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
     PROJECT_NAME:str="Mi Odisea API"
-    # DATABASE_URL:str = "mysql+mysqlconnector://user:secret@localhost:3306/final"
-    DATABASE_URL:str = "postgresql+psycopg2://myuser:mypassword@localhost:5433/apiBrian"
+    DATABASE_HOSTNAME:str=None
+    DATABASE_PORT:int=None
+    DATABASE_PASSWORD:str=None
+    DATABASE_NAME:str=None
+    DATABASE_USERNAME:str=None
+    
+    DATABASE_URL:str 
 
-    SECRET_KEY:str="super_secret_key"
+    SECRET_KEY:str
     ALGORITHM:str="HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 120
 
     BACKEND_CORS_ORIGINS: List[str] = ["*"]
+    
+    MP_ACCESS_TOKEN:str
+    MP_WEBHOOK_URL:str
+         
+    model_config=SettingsConfigDict(
+        env_file=".env", 
+        env_file_encoding="utf-8",
+        extra="ignore"
+        )
     
 settings=Settings()
