8ebea21d582d13c9c66d553d370c35b9ad470b57
Brian Aira <136931601+BrianAira@users.noreply.github.com>
2026-03-02 19:55:36 -0300

    fix(order_router): integrar conexion con pasarela de pago dentro del endpoint checkout

--- app/api/router/order_router.py
+++ app/api/router/order_router.py
@@ -2,48 +2,79 @@ from typing import List
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlmodel import Session
 
+from app.api.dependencies.dependencies import get_payment_repo, get_payment_service
+from app.application.services.mercado_pago import MercadoPagoService
 from app.core.database import get_db
 from app.core.security import get_current_admin_user, get_current_user
 from app.domain.models.order import OrderStatus
 from app.domain.models.user import User, UserRole
-from app.domain.schemas.order import OrderCreate, OrderFilters, OrderResponse
+from app.domain.ports.payment_repository import IPaymentRepository
+from app.domain.schemas.order import CheckoutResponse, OrderCreate, OrderFilters, OrderResponse
+from app.infrastructure.external_apis.mercadopago_repository import MercadoPagoRepository
 from app.infrastructure.repositories.cart import SQLCartRepository
 from app.infrastructure.repositories.order import SQLOrderRepository
 from app.infrastructure.repositories.product import SQLProductRepository
 from app.infrastructure.repositories.user import SQLUserRepository
 from app.infrastructure.repositories.variant_repository import SQLVariantRepository
 from app.application.services.order import OrderService
-
+from app.core.config import settings
 
 router=APIRouter(
     prefix="/orders",
     tags=["Orders"]
 )
-
-def get_order_service(db:Session=Depends(get_db))->OrderService:
+#Dependencia de servicio de ordenes
+def get_order_service(
+    db:Session=Depends(get_db),
+    payment_service:MercadoPagoService=Depends(get_payment_service)
+    # payment_repo:IPaymentRepository=Depends(get_payment_repo)
+    )->OrderService:
     return OrderService(
         order_repo=SQLOrderRepository(db),
         cart_repo=SQLCartRepository(db),
         variant_repo=SQLVariantRepository(db),
         user_repo=SQLUserRepository(db),
-        product_repo=SQLProductRepository(db)
+        product_repo=SQLProductRepository(db),
+        payment_repo=payment_service.payment_provider
     )
     
-@router.post("/", response_model=OrderResponse, status_code=status.HTTP_201_CREATED)
+@router.post(
+    "/", 
+    response_model=CheckoutResponse, 
+    status_code=status.HTTP_201_CREATED)
 def checkout(
     order_data:OrderCreate,
     current_user:User=Depends(get_current_user),
-    service:OrderService=Depends(get_order_service)
+    service:OrderService=Depends(get_order_service),
+    # payment_service:MercadoPagoService=Depends(get_payment_service)
 ):
     try:
-        return service.checkout(
+        #Logica de negocio para crear la orden
+        result= service.checkout(
             user_id=current_user.id,
             direction_id=order_data.shipping_address_id
         )
+        
+        order=result["order"]
+        link=result["payment_url"]
+        #Generacion del link de mp usando la orden recien creada
+        # payment_url=payment_service.start_checkout (order.id, settings.MP_WEBHOOK_URL)
+        
+        return{
+            "order_id":order.id,
+            "total_amount":order.total_amount,
+            "status":order.status,
+            "payment_url":link
+        }
+        
     except ValueError as e:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
     
-@router.get("/my-orders", response_model=List[OrderResponse])
+    #Consulta de errores
+@router.get(
+    "/my-orders", 
+    response_model=List[OrderResponse]
+    )
 def get_my_orders(
     current_user:User=Depends(get_current_user),
     service:OrderService=Depends(get_order_service)
